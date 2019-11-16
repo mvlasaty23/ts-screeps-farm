95bcb67b4c930abf23deb5e4725adb52ca053ef5
Michael Vlasaty <mvlasaty23@gmail.com>
2019-11-16 14:42:54 +0100

    comment out manager

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { CreepManager } from 'managers/creep-manager';
-import { TowerManager } from 'managers/tower-manager';
-import { BuildManager } from 'managers/build-manager';
-import { EventManager } from 'managers/event-manager';
-import { LinkManager } from 'managers/link-manager';
-import { RoomController } from "controller/room.controller";
-import { creepDefs, CreepDefinition } from "definition/creep.definition";
+// import { CreepManager } from 'managers/creep-manager';
+// import { TowerManager } from 'managers/tower-manager';
+// import { BuildManager } from 'managers/build-manager';
+// import { EventManager } from 'managers/event-manager';
+// import { LinkManager } from 'managers/link-manager';
+import { RoomController } from 'controller/room.controller';
+import { creepDefs, CreepDefinition } from 'definition/creep.definition';
 
 export function loop() {
   Object.keys(Game.rooms)
@@ -13,12 +13,12 @@ export function loop() {
     .filter(roomController => roomController)
     .forEach(roomController => roomController.execute());
 
-	EventManager.run();
-	CreepManager.run();
-    TowerManager.run();
-	BuildManager.run();
-	LinkManager.run();
-    
+  // EventManager.run();
+  // CreepManager.run();
+  // TowerManager.run();
+  // BuildManager.run();
+  // LinkManager.run();
+
   if (Game.time % 100 === 50) {
     forgetDeadCreeps(Memory.creeps, Game.creeps);
   }
@@ -30,7 +30,7 @@ function forgetDeadCreeps(creepsInMemory: { [name: string]: any }, creepsInGame:
     Object.keys(creepsInMemory)
       .filter(index => !creepsInGame[index])
       .forEach(index => delete creepsInMemory[index]);
-	}
+  }
 }
 
 function executeStrategies(creep: Creep, strategies: any[]) {
@@ -39,10 +39,10 @@ function executeStrategies(creep: Creep, strategies: any[]) {
     try {
       result = strategy(creep);
       // console.log(`Running ${creep.name} ${strategy.name} ${result}`);
-    } catch(e) {
+    } catch (e) {
       console.log(`Cannot run ${creep.name} ${strategy.name}`, e.stack);
     }
-    if(result === OK || result === ERR_TIRED) {
+    if (result === OK || result === ERR_TIRED) {
       // console.log(`Running ${creep.name} ${strategy.name} ${result}`);
       creep.memory.executingStrategy = strategy.name;
       return true;
@@ -55,31 +55,33 @@ function executeStrategies(creep: Creep, strategies: any[]) {
   return creep.memory.executingStrategy;
 }
 
-function creepsByType(livingCreeps: { [creepName: string]: Creep; }) {
-  return (def) => ({
+function creepsByType(livingCreeps: { [creepName: string]: Creep }) {
+  return def => ({
     def,
-    creeps: Object.keys(livingCreeps).map(key => livingCreeps[key]).filter(creep => creep.memory.role === def.role.name),
+    creeps: Object.keys(livingCreeps)
+      .map(key => livingCreeps[key])
+      .filter(creep => creep.memory.role === def.role.name),
   });
 }
 
-function creepsNotRenewing({def, creeps}) {
-  return({
+function creepsNotRenewing({ def, creeps }) {
+  return {
     def,
     creeps: creeps.filter(creep => def.role.liveStrategy(creep)),
-  });
+  };
 }
 
 function runStrategies(workStrategies) {
-  return (creep) => executeStrategies(creep, workStrategies);
+  return creep => executeStrategies(creep, workStrategies);
 }
 
-function runAllCreeps({def, creeps}) {
+function runAllCreeps({ def, creeps }) {
   creeps.forEach(runStrategies(def.role.workStrategies));
 }
 
-function runCreepsStrategy(creepDefss: CreepDefinition[], livingCreeps: { [creepName: string]: Creep; }) {
+function runCreepsStrategy(creepDefss: CreepDefinition[], livingCreeps: { [creepName: string]: Creep }) {
   creepDefss
-  .map(creepsByType(livingCreeps))
-  .map(creepsNotRenewing)
-  .forEach(runAllCreeps);
+    .map(creepsByType(livingCreeps))
+    .map(creepsNotRenewing)
+    .forEach(runAllCreeps);
 }
